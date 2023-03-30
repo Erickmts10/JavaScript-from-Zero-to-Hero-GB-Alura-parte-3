e383251de98022a72de9c73d21fb5426200e1066
erickmts10 <erick.mts10@gmail.com>
2023-03-30 09:49:53 -0300

    var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// boolleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = Number("456a");
+
+// number
+// string
+console.log(numero + numeroString);
+
+// conversão expícita
